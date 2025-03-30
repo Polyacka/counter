c09092f6942e714c3a675a2c82a7a7610771e74a
Polycka <kurdovblya@gmail.com>
2025-03-30 15:00:34 +0200

    files2

--- javaS/main.js
+++ javaS/main.js
@@ -1,8 +1,39 @@
-//for (var i = 2; i <34 ; i *=2 ){
-  //  console.log(i);
-//}
-var j=2;
-while (j<=64){
-    console.log(j)
-    j *=2;
+let count = 0; // Переменная для хранения числа
+
+function addOne() {
+    count +=2;// Увеличиваем на 1// Выводим
+    updateCounter()
 }
+function remoove(){
+    if (count ===0){
+        alert("Счётчик уже нулевой!");
+    }
+    else{
+        count = 0;
+    }
+    updateCounter()
+}
+function mooveOne(){
+    if (count > 0){
+        count -=2;
+    } else {
+    }
+    updateCounter()
+}
+function updateCounter(){
+    document.getElementById("counter").textContent = count;
+}
+var currentNum="?"
+function random(){
+    currentNum = Math.floor(Math.random()*100)
+    document.getElementById("number").textContent = currentNum;
+}
+function reset(){
+    if (currentNum === "?" ){
+        alert("allready empty")
+    }else {
+        currentNum ="?"
+    }
+    document.getElementById("number").textContent = currentNum;
+}
+
